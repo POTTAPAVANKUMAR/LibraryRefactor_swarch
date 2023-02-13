dd1a4eb294bf4010aa2fc8b4ea25fe1d90a8cd0a
pavankumar potta <pavankumar.potta1998@gmail.com>
2023-02-12 19:13:38 -0500

    core, ui and linkedlist lib changes

---
+++ LINKEDLIST_LIB/LINKEDEXPORTS.h
@@ -0,0 +1,8 @@
+#pragma once
+
+
+#ifdef LINKEDLIST_EXPORTS
+#define LINKEDLIST_API __declspec(dllexport)
+#else
+#define LINKEDLIST_API __declspec(dllimport)
+#endif
---
+++ UI_LIB/uiexports.h
@@ -0,0 +1,9 @@
+#pragma once
+
+
+#ifdef UI_EXPORTS
+#define UI_API __declspec(dllexport)
+#else
+
+#define UI_API __declspec(dllimport)
+#endif
